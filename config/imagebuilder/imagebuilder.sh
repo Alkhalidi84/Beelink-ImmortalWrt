88e08045e655fdf0a59502cccecd145fe24167d6
Alkhalidi84 <137053525+Alkhalidi84@users.noreply.github.com>
2026-04-02 09:24:28 +0300

    Update imagebuilder.sh

--- config/imagebuilder/imagebuilder.sh
+++ config/imagebuilder/imagebuilder.sh
@@ -228,7 +228,6 @@ rebuild_firmware() {
         luci luci-base luci-compat luci-lib-base \
         luci-lib-ip luci-lib-ipkg luci-lib-jsonc luci-lib-nixio luci-mod-admin-full luci-mod-network \
         luci-mod-status luci-mod-system luci-proto-3g luci-proto-ipip luci-proto-ipv6 \
-        luci-proto-ncm luci-proto-openconnect luci-proto-ppp luci-proto-qmi luci-proto-relay \
         \
         luci-app-amlogic \
         luci-app-tinyfm \
